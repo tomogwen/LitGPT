35c20f5337f03ecc9b04282feb5f13a71798dcd0
tomogwen <tom.davies.96@hotmail.co.uk>
2024-03-07 16:12:33 +0000

    update slurm shell script

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -3,29 +3,42 @@
 # --  Set the following! --
 ############################
 # ---- Account Details ----
-QOS = your_slurm_qos 
-ACCOUNT = your_slurm_account
+QOS=your_qos
+ACCOUNT=your_account
 # ---- Time Requested  ---- 
-hours = 1
-mins = 0
-seconds = 0
+hours=1
+mins=0
+seconds=0
 # -- Resources Requested --
-NODES = 2
-GPUS_PER_NODE = 2
-CPUS_PER_GPU = 36
+NODES=2
+GPUS_PER_NODE=2
+CPUS_PER_NODE=4
 # ------ Conda Setup ------
-CONDA_ENVS_DIR = /path/to/envs/
-CONDA_ENV_PATH = $CONDA_ENVS_DIR+="litgpt"
-CONDA_PACKAGES_DIR = /path/to/packages/
+CONDA_ENVS_DIR=/path/to/dir/for/envs/
+CONDA_ENV_PATH="$CONDA_ENVS_DIR""litgpt/"
+CONDA_PACKAGES_DIR=/path/to/store/conda-packages/
 ############################
 # If you want to use wanbd run
 # > wanbd login
 # to add creds to your .netrc
 ############################
 
+sed \
+  -e "s|\$QOS|$QOS|" \
+  -e "s|\$ACCOUNT|$ACCOUNT|" \
+  -e "s|\$hours|$hours|" \
+  -e "s|\$mins|$mins|" \
+  -e "s|\$seconds|$seconds|" \
+  -e "s|\$NODES|$NODES|" \
+  -e "s|\$GPUS_PER_NODE|$GPUS_PER_NODE|" \
+  -e "s|\$CPUS_PER_NODE|$CPUS_PER_NODE|" \
+  -e "s|\$CONDA_ENVS_DIR|$CONDA_ENVS_DIR|" \
+  -e "s|\$CONDA_ENV_PATH|$CONDA_ENV_PATH|" \
+  -e "s|\$CONDA_PACKAGES_DIR|$CONDA_PACKAGES_DIR|" << 'EOF' | sbatch
+#!/bin/bash
 #SBATCH --qos $QOS
 #SBATCH --account $ACCOUNT
-#SBATCH --time $H:$M:$S
+#SBATCH --time $hours:$mins:$seconds
 #SBATCH --nodes $NODES
 #SBATCH --gpus-per-node $GPUS_PER_NODE
 #SBATCH --cpus-per-gpu $CPUS_PER_NODE
@@ -52,3 +65,4 @@ conda activate ${CONDA_ENVS_DIR}
 
 # run train script
 srun litgpt fit --config configs/slurm.yaml --trainer.devices $NODES --trainer.devices $GPUS_PER_NODE --data.train_dataloader_workers $CPUS_PER_NODE --data.val_dataloader_workers $CPUS_PER_NODE
+EOF
